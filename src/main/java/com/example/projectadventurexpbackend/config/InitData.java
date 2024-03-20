91ca903a4cdd11f87a353578e99aab504954f084
tora0001 <tobe.2309@hotmail.com>
2024-03-20 21:03:13 +0100

    Create InitData.java

---
+++ src/main/java/com/example/projectadventurexpbackend/config/InitData.java
@@ -0,0 +1,20 @@
+package com.example.projectadventurexpbackend.config;
+
+import com.example.projectadventurexpbackend.Model.Paintball;
+import com.example.projectadventurexpbackend.Repository.PaintballRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Component
+public class InitData implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
