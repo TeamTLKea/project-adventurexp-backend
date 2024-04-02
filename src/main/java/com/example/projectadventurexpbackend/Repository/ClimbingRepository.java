2c2dab5d01247efed1071626ffb32972502fed4d
Unstabel <Lupe0001@stud.kea.dk>
2024-04-02 15:38:13 +0200

    Test

--- src/main/java/com/example/projectadventurexpbackend/Repository/ClimbingRepository.java
+++ src/main/java/com/example/projectadventurexpbackend/Repository/ClimbingRepository.java
@@ -3,7 +3,9 @@ package com.example.projectadventurexpbackend.Repository;
 import com.example.projectadventurexpbackend.Model.Climbing;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 
 public interface ClimbingRepository extends JpaRepository<Climbing, Integer> {
- //   void deleteByDateBefore(LocalDate cutoffDate);
+ void deleteByDateBefore(LocalDate cutoffDate);
 }
--- src/main/java/com/example/projectadventurexpbackend/Repository/MinigolfRepository.java
+++ src/main/java/com/example/projectadventurexpbackend/Repository/MinigolfRepository.java
@@ -3,6 +3,9 @@ package com.example.projectadventurexpbackend.Repository;
 import com.example.projectadventurexpbackend.Model.Minigolf;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 
 public interface MinigolfRepository extends JpaRepository<Minigolf, Integer> {
+    void deleteByDateBefore(LocalDate cutoffDate);
 }
